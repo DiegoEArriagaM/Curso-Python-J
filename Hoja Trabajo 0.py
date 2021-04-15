18551bd224f7fdc88934b08d45f8a9fbf1b44059
Diego Enrique Arriaga Meléndez <3017419640101@ingenieria.usac.edu.gt>
2021-04-15 14:47:43 -0600

    Clase 15/04/2021

---
+++ Hoja Trabajo 0.py
@@ -0,0 +1,10 @@
+print("Calculo del Indice de masa corporal (IMC)")
+#Peticion del peso y la altura
+peso=int(input("Por favor ingrese su peso en Kilogramos: "))
+altura=float(input("Por favor ingrese su altura en metros: "))
+
+#Calcular IMC
+imc=peso/(altura**2)
+
+#Mostrar el resultaro
+print("Tu índice de masa corporal es ",round(imc,2))
